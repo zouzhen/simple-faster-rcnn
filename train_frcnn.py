abe6909e409406c24709f7932f32289ec8417873
zouzhen <996736369@qq.com>
2018-12-17 09:54:26 +0800

    路径更新

--- train_frcnn.py
+++ train_frcnn.py
@@ -33,7 +33,7 @@ sys.setrecursionlimit(40000)
 
 parser = OptionParser()
 
-parser.add_option("-p", "--path", dest="train_path", help="Path to training data.", default='/home/duoda/WorkSpace/VSCode/DeepLearning/keras-frcnn-master/VOCdevkit')
+parser.add_option("-p", "--path", dest="train_path", help="Path to training data.", default='/home/code/fasterRCNN/VOCdevkit')
 parser.add_option("-o", "--parser", dest="parser", help="Parser to use. One of simple or pascal_voc",
 				default="pascal_voc")
 parser.add_option("-n", "--num_rois", type="int", dest="num_rois", help="Number of RoIs to process at once.", default=32)
@@ -46,7 +46,7 @@ parser.add_option("--num_epochs", type="int", dest="num_epochs", help="Number of
 parser.add_option("--config_filename", dest="config_filename", help=
 				"Location to store all the metadata related to the training (to be used when testing).",
 				default="config.pickle")
-parser.add_option("--output_weight_path", dest="output_weight_path", help="Output path for weights.", default='/home/duoda/WorkSpace/Blast/BlastBackStage/Algorithms/BlastRecognition/model_frcnn.hdf5')
+parser.add_option("--output_weight_path", dest="output_weight_path", help="Output path for weights.", default='/home/code/fasterRCNN')
 parser.add_option("--input_weight_path", dest="input_weight_path", help="Input path for weights. If not specified, will try to load default weights provided by keras.")
 
 (options, args) = parser.parse_args()
