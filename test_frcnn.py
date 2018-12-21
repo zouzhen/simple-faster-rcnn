4be59666eefac309eb6e2730a182d4699f69dcae
zouzhen <996736369@qq.com>
2018-12-21 16:35:47 +0800

    更改测试路径

--- test_frcnn.py
+++ test_frcnn.py
@@ -26,7 +26,7 @@ sys.setrecursionlimit(40000)
 
 parser = OptionParser()
 
-parser.add_option("-p", "--path", dest="test_path", help="Path to test data.", default='/home/duoda/WorkSpace/VSCode/DeepLearning/keras-frcnn-master/demo')
+parser.add_option("-p", "--path", dest="test_path", help="Path to test data.", default='/home/code/fasterRCNN/demo')
 parser.add_option("-n", "--num_rois", type="int", dest="num_rois",
 				help="Number of ROIs per iteration. Higher means more memory use.", default=32)
 parser.add_option("--config_filename", dest="config_filename", help=
